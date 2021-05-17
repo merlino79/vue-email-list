42803ed8415240720f7a6b468acd91665a41188f
Merlino79 <alessandro.ciancio@hotmail.com>
2021-05-17 17:55:29 +0200

    css

--- asset/js/script.js
+++ asset/js/script.js
@@ -21,6 +21,7 @@ const root = new Vue({
         for (i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((resp) => {
+                    console.log(resp);
                     console.log(resp.data.response)
                     this.utente.push(resp.data.response)
                 })
