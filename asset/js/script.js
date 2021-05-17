85fb085e9d9b86600801fe45ba6d8ea80deecdb4
Merlino79 <alessandro.ciancio@hotmail.com>
2021-05-17 17:20:30 +0200

    impostazioni inizio

---
+++ asset/js/script.js
@@ -0,0 +1,28 @@
+const root = new Vue({
+
+    el: '#root',
+    data: {
+
+        
+
+    },
+    methods: {
+
+    },
+
+    created() {
+
+    },
+
+    mounted() {
+
+        
+
+
+
+
+    },
+
+});
+
+console.log(root);
