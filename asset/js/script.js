437da0362e90b1306be721b5cdb5187a6d8a2ccb
Merlino79 <alessandro.ciancio@hotmail.com>
2021-05-17 17:45:36 +0200

    stampa a video con v-for in html della lista creata con il ciclo for nel mounted

--- asset/js/script.js
+++ asset/js/script.js
@@ -18,14 +18,19 @@ const root = new Vue({
 
     mounted() {
 
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((resp) => {
-                console.log(resp.data.response)
-                this.utente.push(resp.data.response)
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+        for (i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp) => {
+                    console.log(resp.data.response)
+                    this.utente.push(resp.data.response)
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+
+        }
+
+
 
 
 
